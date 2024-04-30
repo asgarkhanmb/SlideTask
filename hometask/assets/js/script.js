5690834f56c527efbc90b89ac8e363b607636bd9
unknown <asgarkhanmb@code.edu.az>
2024-04-30 17:50:19 +0400

    Slide Task

---
+++ hometask/assets/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let slideNumber = 1;
+showSlides(slideNumber);
+
+
+function plusSlides(n) {
+  showSlides(slideNumber += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideNumber = 1}
+  if (n < 1) {slideNumber = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideNumber-1].style.display = "block";
+
+}
